695ed37e16e77c29afddc6f571a1ae627db96155
Isabella Wang <wangxbella0108@gmail.com>
2019-09-22 23:35:16 -0400

    Updates

---
+++ precache-manifest.e042b63edba8bbe60d21644b9b3c6db1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ea00f148f2e1859c84f60e55efb97f",
+    "url": "/IdealationVote/index.html"
+  },
+  {
+    "revision": "71f6c920868ec47bc515",
+    "url": "/IdealationVote/static/css/main.bbfcb933.chunk.css"
+  },
+  {
+    "revision": "f6d8e48bd5b6b7c49c23",
+    "url": "/IdealationVote/static/js/2.d6a86d2b.chunk.js"
+  },
+  {
+    "revision": "71f6c920868ec47bc515",
+    "url": "/IdealationVote/static/js/main.1f6ac881.chunk.js"
+  },
+  {
+    "revision": "f23deb9238d359cea6fe",
+    "url": "/IdealationVote/static/js/runtime-main.4163226c.js"
+  },
+  {
+    "revision": "1b5dad28d30f71fb24c50379edc22259",
+    "url": "/IdealationVote/static/media/bg.1b5dad28.png"
+  },
+  {
+    "revision": "12482c378a6fc0ad1e7bbf8c62846ab2",
+    "url": "/IdealationVote/static/media/like.12482c37.png"
+  },
+  {
+    "revision": "c8bd73a0391af755198d84b3af22cb32",
+    "url": "/IdealationVote/static/media/mindmap.c8bd73a0.png"
+  }
+]);
